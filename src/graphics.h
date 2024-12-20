4d4a01952325eacdf0b49b141307165122583098
Bakiki <adeister@gmx.net>
2024-12-20 18:41:30 +0100

    adding Vulkan instance

---
+++ src/graphics.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<vulkan/vulkan.h>
+#include <glfw_window.h>
+
+namespace sreng {
+
+  class Graphics final{
+
+  public:
+    Graphics(gsl::not_null<Window*> window);
+    ~Graphics();
+  private:
+    void InitializeVulkan();
+    void CreateInstance();
+
+    gsl::span<gsl::czstring> GetSuggestedExtensions();
+
+    VkInstance instance_ = nullptr;
+    gsl::not_null<Window*> window_;
+  };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <glfw_monitor.h>
 #include <glfw_window.h>
 #include <precomp.h>
+#include <graphics.h>
 
 
 
@@ -13,7 +14,9 @@ std::int32_t main()
   sreng::Window window("Sky Render",{800,600});
 
   window.TryMoveToMonitors(1);
-  
+
+  sreng::Graphics graphics(&window);
+
   while (!window.ShouldClose()) {
     glfwPollEvents();
   }
